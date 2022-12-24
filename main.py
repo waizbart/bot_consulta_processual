35c927249e5ad8fa121ec6b5e3634ceea17a94a5
waizbart <guilhermewaizbart@gmail.com>
2022-12-24 09:18:43 -0300

    array de dtas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkcalendar import DateEntry
-from  datetime import date
+from datetime import date, datetime
+from process import savePdfs
 
 root = tk.Tk(baseName="root")
 
@@ -11,21 +12,37 @@ root.configure(background='#262626')
 root.columnconfigure(0, weight=2)
 root.columnconfigure(1, weight=2)
 
+sel1 = tk.StringVar()
+sel2 = tk.StringVar()
+
 lbX1 = tk.Label(root, text="Data inicial: ",
                 background='#262626', foreground="white")
 lbX1.grid(column=0, row=1, pady=10)
-cal1 = DateEntry(root,selectmode='day')
+cal1 = DateEntry(root, selectmode='day', textvariable=sel1)
 cal1.grid(column=0, row=2, pady=5)
 cal1.set_date(date.today())
 
 lbY2 = tk.Label(root, text="Data final: ",
                 background='#262626', foreground="white")
 lbY2.grid(column=1, row=1, pady=10)
-cal2 = DateEntry(root,selectmode='day')
+cal2 = DateEntry(root, selectmode='day', textvariable=sel2)
 cal2.grid(column=1, row=2, pady=5)
 cal2.set_date(date.today())
 
-init_btn = tk.Button(root, text="INICIAR DOWNLOAD DE PROCESSOS", command="init_inspector", width=40,
+def init():
+    initialDate = sel1.get()
+    finalDate = sel2.get()
+
+    initialDay, initialMonth, finalYear = initialDate.split('/')
+    initialDate = datetime(int(finalYear), int(initialMonth), int(initialDay))
+
+    finalDay, finalMonth, finalYear = finalDate.split('/')
+    finalDate = datetime(int(finalYear), int(finalMonth), int(finalDay))
+
+    print(initialDate, finalDate)
+    savePdfs(initialDate, finalDate)
+
+init_btn = tk.Button(root, text="INICIAR DOWNLOAD DE PROCESSOS", command=init, width=40,
                      height=2, bg="#2E4159", activebackground="#8F8EBF", disabledforeground="yellow", fg="white")
 init_btn.grid(columnspan=4, sticky=tk.W+tk.E, pady=15, padx=10)
 
--- process.py
+++ process.py
@@ -3,6 +3,7 @@ import fitz
 import os
 from time import sleep
 from utils import getCaptchaToken
+from datetime import timedelta, datetime
 
 headers = {
     "accept": "application/json, text/plain, */*",
@@ -17,51 +18,60 @@ headers = {
     "Referrer-Policy": "no-referrer-when-downgrade",
 }
 
-tokenCaptcha = "a72ddd0094ad56fd16f6d625feb4562ab703907a8a071daaf7892283950d4d0a"
-
-idOj = "2"
-data = "2022-12-19"
-
 urlOrgaos = "https://pje.trt2.jus.br/pje-consulta-api/api/orgaosjulgadores?somenteOJCs=true"
 
-tipos = ["Inicial (rito sumaríssimo)", "Inicial por videoconferência", "Una", "Una (rito sumaríssimo)"]
+tipos = ["Inicial (rito sumaríssimo)", "Inicial por videoconferência",
+         "Una", "Una (rito sumaríssimo)"]
+
 
 def savePdfs(initialDate, finalDate):
+    dif = finalDate - initialDate
+
+    lista_datas = []
+    for i in range(dif.days + 1):
+        day = initialDate + timedelta(days=i)
+        lista_datas.append(day)
+
+    lista_datas = [datetime.strftime(dt, format="%Y-%m-%d")
+                   for dt in lista_datas]
+
+    print(lista_datas)
+    return
     orgaos = requests.get(urlOrgaos, headers=headers)
     orgaos = orgaos.json()
 
-    
+    for data in lista_datas:
+        for orgao in orgaos:
+            if orgao.get("id"):
+                id = orgao["id"]
 
-    for orgao in orgaos:
-        if orgao.get("id"):
-            id = orgao["id"]
+                url = "https://pje.trt2.jus.br/pje-consulta-api/api/audiencias?pagina=1&tamanhoPagina=100&idOj=" + \
+                    str(id) + "&data=" + data
+                req = requests.get(url, headers=headers)
+                req = req.json()
 
-            url = "https://pje.trt2.jus.br/pje-consulta-api/api/audiencias?pagina=1&tamanhoPagina=100&idOj=" + \
-                str(id) + "&data=" + data
-            req = requests.get(url, headers=headers)
-            req = req.json()
+                if req.get("resultado"):
+                    for item in req["resultado"]:
+                        if item["tipo"] in tipos:
+                            idProcesso = item["idProcesso"]
 
-            if req.get("resultado"):
-                for item in req["resultado"]:
-                    if item["tipo"] in tipos:
-                        idProcesso = item["idProcesso"]
+                            captchaToken = getCaptchaToken(idProcesso)
 
-                        captchaToken = getCaptchaToken(idProcesso)
+                            file = requests.get(
+                                'https://pje.trt2.jus.br/pje-consulta-api/api/processos/' + idProcesso + '/documentos/283356716?tokenCaptcha=' + captchaToken, allow_redirects=True, headers=headers)
 
-                        file = requests.get(
-                            'https://pje.trt2.jus.br/pje-consulta-api/api/processos/' + idProcesso + '/documentos/283356716?tokenCaptcha=' + captchaToken, allow_redirects=True, headers=headers)
+                            download = open(
+                                item["numeroProcesso"] + '.pdf', 'wb')
+                            download.write(file.content)
+                            download.close()
 
-                        download = open(item["numeroProcesso"] + '.pdf', 'wb')
-                        download.write(file.content)
-                        download.close()
+                            keyWords = 0
 
-                        keyWords = 0
+                            with fitz.open(item["numeroProcesso"] + '.pdf') as pdf:
+                                for pagina in pdf:
+                                    if 'perícia' in pagina.get_text():
+                                        keyWords += 1
+                                        break
 
-                        with fitz.open(item["numeroProcesso"] + '.pdf') as pdf:
-                            for pagina in pdf:
-                                if 'perícia' in pagina.get_text():
-                                    keyWords += 1
-                                    break
-
-                        if keyWords < 2:
-                            os.remove(item["numeroProcesso"] + '.pdf')
+                            if keyWords < 2:
+                                os.remove(item["numeroProcesso"] + '.pdf')
